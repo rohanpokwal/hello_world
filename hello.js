d484cefc4fce6a8d707692263c6eb8f60deb4c8f
Rohan Pokwal <rohan.pokwal@gmail.com>
2024-04-23 15:44:26 -0500

    First JS Program

---
+++ hello.js
@@ -0,0 +1,8 @@
+//turn on strict mide ti help us with best practices and error checking
+"use strict";
+
+//creating a variable called message to hold the message "Hellow World"
+let message = "Helloww World";
+
+//displaying the message variable in the console
+console.log(message);
